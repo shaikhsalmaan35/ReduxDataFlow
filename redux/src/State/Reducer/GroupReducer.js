7ab11dd0e256b15af6efaa51febbf88866020f79
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-17 00:14:17 +0530

    Redux-part-state-4-component

---
+++ redux/src/State/Reducer/GroupReducer.js
@@ -0,0 +1,27 @@
+const groupState = {
+    group: [{
+            fname: "Aamer",
+            lname: "sohel",
+            pic: "",
+            position: "UI Developer",
+            techno: "React",
+        },
+        {
+            fname: "Aamer",
+            lname: "sohel",
+            pic: "",
+            position: "UI Developer",
+            techno: "React",
+        },
+    ]
+}
+export const groupReducers = (state = groupState, action) => {
+    switch (action.type) {
+        case "ADD_NEW_GROUP":
+            return {
+                ...state, group: [action.group, ...state.group]
+            };
+        default:
+            return state;
+    }
+}
