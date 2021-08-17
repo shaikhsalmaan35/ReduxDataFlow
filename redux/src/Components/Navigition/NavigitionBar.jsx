e98bcf5079a821ff507322740d6724e86941894f
shaikhsalmaan35 <71723728+shaikhsalmaan35@users.noreply.github.com>
2021-08-17 15:44:36 +0530

    completed-redux-flow-aaded-navbar

--- redux/src/Components/Navigition/NavigitionBar.jsx
+++ redux/src/Components/Navigition/NavigitionBar.jsx
@@ -2,5 +2,20 @@ import React from "react";
 import { Navbar, Container, NavLink, Nav } from "react-bootstrap";
 
 export const NavigitionBar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <NavLink to="/">
+            <Navbar.Brand href="#home">Redux</Navbar.Brand>
+          </NavLink>
+          <Nav className="me-auto">
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/Fetching">Fetching</NavLink>
+            <NavLink to="/About US">About US</NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
